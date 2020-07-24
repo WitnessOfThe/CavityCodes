740ad34cd90767c872f7f40302aa443b908ac0c3
WitnessOfThe <puzurdan@gmail.com>
2020-07-24 20:06:01 +0100

    Dogon

---
+++ Problems/Chi_3/Pure_LLE/Solution Search/Dynamics/Saving Data/Chi_3_LLE_Dyn_Saving.m
@@ -0,0 +1,13 @@
+function Chi_3_LLE_Dyn_Saving(Save,Delta,Eq,ni,F_e)
+
+    if ni == 0
+        
+        Save =  
+        
+    elseif (mod(ni,Delta.NN) == 0) && (ni =~ 0)
+        
+        
+        
+    end
+    
+end
