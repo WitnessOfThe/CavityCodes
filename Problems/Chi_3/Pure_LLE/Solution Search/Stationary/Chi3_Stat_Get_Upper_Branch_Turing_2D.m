38d1c13dff8eecd0b47e709dda95dd93d79d9119
WitnessOfThe <puzurdan@gmail.com>
2020-11-18 17:57:48 +0000

    2D Tracking of Upper branch is separated

--- Problems/Chi_3/Pure_LLE/Solution Search/Stationary/Chi3_Stat_Get_Upper_Branch_Turing_2D.m
+++ Problems/Chi_3/Pure_LLE/Solution Search/Stationary/Chi3_Stat_Get_Upper_Branch_Turing_2D.m
@@ -1,4 +1,4 @@
-function [CaF_1D_Upper,CaF_1D_Lower] =  Chi3_Stat_Get_Branch_Turing_2D(ReS,mu,N_mode,Power_Lim,Delta_Lim,N_Power_Res,N_Delta_Res)
+function [CaF_1D_Upper] =  Chi3_Stat_Get_Upper_Branch_Turing_2D(ReS,mu,N_mode,Power_Lim,Delta_Lim,N_Power_Res,N_Delta_Res)
 
     ReS.Stat.Par.bot_boundary     = min(Delta_Lim); % bottom boundary for delta to search
     ReS.Stat.Par.top_boundary     = max(Delta_Lim); % top boundary for delta to search
@@ -12,9 +12,9 @@ function [CaF_1D_Upper,CaF_1D_Lower] =  Chi3_Stat_Get_Branch_Turing_2D(ReS,mu,N_
           Res = ReS;          
           Res.CW.In.delta = delta_s_vec(i)*Res.CW.In.kappa;          
           Res.CW          = Res.Met.T_MI (Res.CW,N_mode);
-          power_s_vec(i)  = Res.CW.In.W_MI_Tongue(2,mu+1)*0.99;
+          power_s_vec(i)  = Res.CW.In.W_MI_Tongue(1,mu+1)*1;
     end
-    
+    delta_s_vec = delta_s_vec + 0.5;
 
     parfor  i = 1:N_Power_Res
         
@@ -23,7 +23,6 @@ function [CaF_1D_Upper,CaF_1D_Lower] =  Chi3_Stat_Get_Branch_Turing_2D(ReS,mu,N_
         Res.Stat.In.P     = power_s_vec(i);
         [Upper,Lower] = Chi3_Stat_Get_Branch_Turing(Res,mu,N_mode);
         CaF_1D_Upper(i) = Upper;
-        CaF_1D_Lower(i) = Lower;
         
     end
     
