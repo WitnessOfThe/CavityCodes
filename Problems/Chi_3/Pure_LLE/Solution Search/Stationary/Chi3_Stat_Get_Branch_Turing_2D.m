080cd1f6e621c554ef1bf6c98bfece2c4aeefe80
WitnessOfThe <33790455+WitnessOfThe@users.noreply.github.com>
2020-11-18 18:00:18 +0000

    Revert "2D Tracking of Upper branch is separated"
    This reverts commit 38d1c13dff8eecd0b47e709dda95dd93d79d9119.

--- Problems/Chi_3/Pure_LLE/Solution Search/Stationary/Chi3_Stat_Get_Branch_Turing_2D.m
+++ Problems/Chi_3/Pure_LLE/Solution Search/Stationary/Chi3_Stat_Get_Branch_Turing_2D.m
@@ -2,7 +2,7 @@ function [CaF_1D_Upper,CaF_1D_Lower] =  Chi3_Stat_Get_Branch_Turing_2D(ReS,mu,N_
 
     ReS.Stat.Par.bot_boundary     = min(Delta_Lim); % bottom boundary for delta to search
     ReS.Stat.Par.top_boundary     = max(Delta_Lim); % top boundary for delta to search
-    ReS.Stat.Par.i_max            = round((ReS.Stat.Par.top_boundary- ReS.Stat.Par.bot_boundary)/ReS.Stat.Par.first_step);
+    ReS.Stat.Par.i_max            = (ReS.Stat.Par.top_boundary- ReS.Stat.Par.bot_boundary)/ReS.Stat.Par.first_step;
 
 %%
     delta_s_vec = linspace(min(Delta_Lim),max(Delta_Lim),N_Power_Res);
