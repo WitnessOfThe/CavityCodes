d552c702162404029ec0c92b4cf30b0b6c778d14
WitnessOfThe <puzurdan@gmail.com>
2021-07-21 12:03:40 +0100

    Upload

--- Problems/Real_Problems/Bloch_Matrices/SolitonCrystalPaper/CrystalManipulations/Chi3_CrystalCreator.m
+++ Problems/Real_Problems/Bloch_Matrices/SolitonCrystalPaper/CrystalManipulations/Chi3_CrystalCreator.m
@@ -1,16 +1,21 @@
-function R = Chi3_CrystalCreator(R,MP)
+function [Stat,coeff] = Chi3_CrystalCreator(R,MP,i)
 
     R.Stat.Par.Equation_string      = 'Kerr_Full_Dispersion';    
     R.Stat.Met.Newton               = @Newton_Manual_bicgstab;
 
-    R.Stat(1).Par.Newton_tol       = 3E-11;  
+    R.Stat(1).Par.Newton_tol       = 1E-10;  
     R.Stat(1).Par.Stability        = 0;    
     R.Stat(1).Par.Newton_iter      = 200;
 
-    SP = Chi3_CrystalPathGenerator(R.Stat,MP);
     
     [R,coeff] = Chi3_Stat_StartFromCW(R);
     Stat = R.Stat;
-    save(strcat(SP,'Starter.mat'),'Stat');
-    
+    if i == 0
+        SP = Chi3_CrystalPathGenerator(R.Stat,MP);
+        save(strcat(SP,'Starter.mat'),'Stat');
+    else
+%        SP = Chi3_CrystalPathGenerator(R.Stat,MP);
+        NaiSt= IndexToChar(i+26^2*27+26);
+        save(strcat(MP,NaiSt{1},'.mat'),'Stat');
+    end
 end
